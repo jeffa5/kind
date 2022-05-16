f2fd5558cc71cf8f3c1245e2b45893c980365d9b
Andrew Jeffery <dev@jeffas.io>
2022-05-16 16:46:12 +0100

    Add nix config

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Kubernetes performance tests";
+
+  inputs = {
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        packages.clusterloader2 = pkgs.buildGoModule {
+          pname = "clusterloader2";
+          version = "0.1.0";
+          src = ./clusterloader2;
+          vendorSha256 = "sha256-dk9LdI+VnJlP13FWv1F0xubT9YKu5m9wQEYKTD+nuIw=";
+          postInstall = ''
+            mv $out/bin/cmd $out/bin/clusterloader2
+          '';
+          runTests = false;
+        };
+
+        defaultPackage = self.packages.${system}.clusterloader2;
+
+        apps.clusterloader2 = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.clusterloader2;
+        };
+
+        defaultApp = self.apps.${system}.clusterloader2;
+
+        devShell = pkgs.mkShell {
+          packages = with pkgs;
+            [
+              go_1_16
+              kind
+              k9s
+              kubectl
+              helm
+
+              nixpkgs-fmt
+              rnix-lsp
+            ];
+        };
+      });
+}
